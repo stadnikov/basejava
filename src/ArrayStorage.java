54b3cbc3fd547e8ee7dab6d3ec5529ca41b8ccf3
stadnikov <zeroserge@gmail.com>
2022-06-03 10:40:50 +0300

    Реализован класс ArrayStorage в соответствии с задачей.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,68 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        int counter = 0;
+        while (storage[counter] != null) {
+            storage[counter] = null;
+            counter++;
+        }
     }
 
     void save(Resume r) {
+        int counter = 0;
+        while (storage[counter] != null) {
+            counter++;
+        }
+        storage[counter] = r;
     }
 
     Resume get(String uuid) {
+        int counter = 0;
+        while (storage[counter] != null) {
+            if (storage[counter].uuid.equals(uuid)) {
+                return storage[counter];
+            }
+            counter++;
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int counter = 0;
+
+        int itemToRemove = 0;
+
+        while (storage[counter] != null) {
+            if (storage[counter].uuid.equals(uuid)) {
+                itemToRemove = counter;
+            }
+            counter++;
+        }
+
+        if (counter > 0) {
+            storage[itemToRemove] = storage[counter - 1];
+            storage[counter - 1] = null;
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int counter = 0;
+        while (storage[counter] != null) {
+            counter++;
+        }
+        Resume[] tempResumeArray = new Resume[counter];
+        System.arraycopy(storage, 0, tempResumeArray, 0, counter);
+        return tempResumeArray;
     }
 
     int size() {
-        return 0;
+        int counter = 0;
+        while (storage[counter] != null) {
+            counter++;
+        }
+        return counter;
     }
 }
